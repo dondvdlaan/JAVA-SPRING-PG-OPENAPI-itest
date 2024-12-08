7f378f1f548256fed2b88d8aceb1c102dddc7d03
dondvdlaan <dondvdlaan@gmail.com>
2024-12-08 12:55:00 +0100

    DCMi-002: Static WireMockserver w/o Spring runs

---
+++ src/test/java/dev/manyroads/StartWireMockServer.java
@@ -0,0 +1,15 @@
+package dev.manyroads;
+
+import dev.manyroads.wiremock.StartWM;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//@SpringBootApplication
+public class StartWireMockServer {
+
+    public static void main(String[] args) {
+        //SpringApplication.run(StartWireMockServer.class, args);
+        StartWM.run();
+    }
+
+}
