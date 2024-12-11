8e4654e13132994c2a5e518043ed406f5eedec11
dondvdlaan <dondvdlaan@gmail.com>
2024-12-11 14:23:51 +0100

    DCMi-003: customer processing client stub

--- src/test/java/dev/manyroads/steps/WireMockSteps.java
+++ src/test/java/dev/manyroads/steps/WireMockSteps.java
@@ -5,21 +5,31 @@ import com.github.tomakehurst.wiremock.WireMockServer;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
 import static com.github.tomakehurst.wiremock.client.WireMock.get;
+import static com.github.tomakehurst.wiremock.client.WireMock.post;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
 import static com.github.tomakehurst.wiremock.client.WireMock.urlMatching;
 import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.options;
 
-
 public class WireMockSteps {
 
-    @Given("remote data source delivers {string}")
-    public void theRemoteDataSourceDeliversFollowingVehicle(String vehicle) {
+    WireMockServer wireMockServer;
 
+    @Given("admin client source delivers {string}")
+    public void adminClientDeliversFollowingVehicle(String vehicle) {
         int port = 7090;
-        WireMockServer wireMockServer = new WireMockServer(options().port(port));
+        this.wireMockServer = new WireMockServer(options().port(port));
         wireMockServer.start();
+
         wireMockServer.stubFor(get(urlMatching("/vehicles/([0-9]*)"))
                 .willReturn(aResponse().withHeader("Content-Type", "application/json")
                         .withStatus(200)
                         .withBody(vehicle)));
+    }
+
+    @Given("customer process client accepts charge")
+    public void customerProcessClientAcceptsCharge() {
+        wireMockServer.stubFor(post(urlEqualTo("/v1/process_charge"))
+                .willReturn(aResponse().withHeader("Content-Type", "application/json")
+                        .withStatus(200)));
     }
 }
