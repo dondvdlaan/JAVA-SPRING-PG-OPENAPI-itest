894619b2664ee5278eb03d35a36e47cd1225a253
dondvdlaan <dondvdlaan@gmail.com>
2024-12-13 15:04:03 +0100

    DCMi-005: Preparing Jenkins pipeline

--- src/test/java/dev/manyroads/steps/StepDefinitions.java
+++ src/test/java/dev/manyroads/steps/StepDefinitions.java
@@ -7,9 +7,8 @@ import io.restassured.RestAssured;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
-import lombok.extern.slf4j.Slf4j;
-import wiremock.net.minidev.json.JSONObject;
 
+import wiremock.net.minidev.json.JSONObject;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -18,14 +17,13 @@ public class StepDefinitions {
     Response response;
 
     @When("Start Decom with matter request {int} {string}")
-    public void startDCMWithMatterRequest(int customerNr,String matterNr) {
+    public void startDCMWithMatterRequest(int customerNr, String matterNr) {
 
         RestAssured.baseURI = "http://localhost:8080/v1";
         RequestSpecification request = RestAssured.given();
         request.header("Content-Type", "application/json");
         JSONObject jsonObject = new JSONObject();
         jsonObject.put("customerNr", customerNr);
-        System.out.println("matterNr: "+ matterNr);
         jsonObject.put("matterNr", matterNr);
         request.body(jsonObject.toJSONString());
 
@@ -34,15 +32,15 @@ public class StepDefinitions {
     }
 
     @Then("Decom returns {int}")
-    public void theScenarioPasses(int customerNr ) {
-        assertEquals(Integer.toString(customerNr),getJsonPath(response,"customerNr"));
+    public void theScenarioPasses(int customerNr) {
+        assertEquals(Integer.toString(customerNr), getJsonPath(response, "customerNr"));
     }
 
+    // sub methods
     static String getJsonPath(Response response, String key) {
         String complete = response.asString();
         JsonPath js = new JsonPath(complete);
         return js.get(key).toString();
     }
-
 
 }
