cb47aad0d928ab65c2e2b7c182d993713a854ddf
dondvdlaan <dondvdlaan@gmail.com>
2024-12-17 14:19:18 +0100

    DCMi-007: Created intermediate report status feature

--- src/test/java/dev/manyroads/RunSpringTest.java
+++ src/test/java/dev/manyroads/RunSpringTest.java
@@ -1,15 +1,13 @@
 package dev.manyroads;
 
-import dev.manyroads.steps.decom.StartDecomStep;
-import io.cucumber.spring.CucumberContextConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
-public class RunSpring {
+public class RunSpringTest {
 
     public static void main(String[] args) {
-        ApplicationContext context = SpringApplication.run(RunSpring.class);
+        ApplicationContext context = SpringApplication.run(RunSpringTest.class);
     }
 }
