9f30de3aabd047c9d2104d9909042048ca20224e
dondvdlaan <dondvdlaan@gmail.com>
2024-12-17 15:36:01 +0100

    DCMi-007: Created intermediate report status feature

--- src/test/java/dev/manyroads/entities/MatterResponseTestRepo.java
+++ src/test/java/dev/manyroads/entities/MatterResponseTestRepo.java
@@ -2,9 +2,7 @@ package dev.manyroads.entities;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
 import java.util.Optional;
-import java.util.UUID;
 
 public interface MatterResponseTestRepo extends JpaRepository<MatterResponseTestEntity,Integer> {
 
